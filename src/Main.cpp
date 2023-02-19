f8993077e9762e661868283cfcc07cf19b0bd28d
George Fotopoulos <xorz57@gmail.com>
2023-02-19 22:17:01 +0200

    Add Profiler Class

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include "Profiler.hpp"
+
+void f0();
+
+void f1();
+
+void f2();
+
+void f0() {
+    PROFILE_FUNCTION();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    f1();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+}
+
+void f1() {
+    PROFILE_FUNCTION();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    f2();
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+}
+
+void f2() {
+    PROFILE_FUNCTION();
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    PROFILE_FUNCTION();
+    std::this_thread::sleep_for(std::chrono::milliseconds(400));
+    f0();
     return 0;
 }
---
+++ src/Profiler.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cstdio>
+#include <thread>
+
+class Profiler {
+public:
+    Profiler() = delete;
+
+    explicit Profiler(const char *name) : mName(name) {
+        mT0 = std::chrono::high_resolution_clock::now();
+        std::printf("Entered %s\n", mName);
+    }
+
+    Profiler(const Profiler &) = delete;
+
+    Profiler(Profiler &&) = delete;
+
+    ~Profiler() {
+        mT1 = std::chrono::high_resolution_clock::now();
+        mDuration = std::chrono::duration_cast<std::chrono::microseconds>(mT1 - mT0).count();
+        std::printf("Left %s after %llu us\n", mName, mDuration);
+    }
+
+    Profiler &operator=(const Profiler &) = delete;
+
+    Profiler &operator=(Profiler &&) = delete;
+
+private:
+    const char *mName = nullptr;
+    unsigned long long mDuration = 0;
+    std::chrono::time_point<std::chrono::high_resolution_clock> mT0;
+    std::chrono::time_point<std::chrono::high_resolution_clock> mT1;
+};
+
+#define PROFILE_FUNCTION() Profiler profiler(__func__)
